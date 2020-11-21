32b25c5b6b34957fe4a0a391a88e609e32fd9bb0
victrslx <joaoslx1@gmail.com>
2020-11-21 20:06:04 -0300

    loop 1000 1500

---
+++ 1000_1500/src/pkg1000_1500/Main.java
@@ -0,0 +1,16 @@
+package pkg1000_1500;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i; 
+
+        i = 1000;
+        do{
+        i = i + 2;
+        System.out.println(i);
+        }while (i<1500);
+    }
+    
+}
